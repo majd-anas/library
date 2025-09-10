4efd8bf4a475aec65680519cae6cf837b9ba20be
majd anas <majdanas2004@gmail.com>
2025-09-11 01:20:24 +0300

    feat: adding books to the cards

--- script.js
+++ script.js
@@ -4,31 +4,59 @@ const cancel=document.querySelector("#cancel");
 const form=document.querySelector("form");
 const submitBook=document.querySelector("#submitBook")
 
-function Book(title,author,read,id){
+
+function Book(title,author,pageNo,id){
     this.title=title;
     this.author=author;
     this.id=id;
-    this.read=read;
+    this.pageNo=pageNo;
+
 }
 
 
-function addBookToLibrary(title,author,read){
+function addBookToLibrary(){
 
     submitBook.addEventListener("click",()=>{
+        console.log("clicked submit");
         let title=document.querySelector("#bookTitle").value;
         let author=document.querySelector("#bookAuthor").value;
         let pageNo=document.querySelector("#bookPages").value;
-        let book=new Book(title,author,read,crypto.randomUUID());
+        let book=new Book(title,author,pageNo,crypto.randomUUID());
+        let id=book.id;
         lib.push(book);
+        displayLibrary(title,author,pageNo,id);
     });
-    
 
+    
 }
 
-function displayLibrary(){
-    lib.forEach((book)=>{
-        
-    });
+function displayLibrary(title,author,pageNo,id){
+
+    const container=document.querySelector(".container");
+
+    const card=
+    
+    `
+        <div class="container" data-id="${id}" >
+        <div class="card">
+            <div class="titleBar">${title}</div>
+            <div class="content">
+                 <ul>
+                    <li>Author ${author} </li>
+                    <li>Pages ${pageNo} </li>
+                 </ul>
+            </div>
+            <div class="options">
+                <ul>
+                    <button type="button" id="remove" name="remove">Remove</button>
+                    <button type="button" id="edit" name="edit">Edit</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    `;
+    container.innerHTML+=card;
+
 }
 
 function addBookForm(){
@@ -48,3 +76,4 @@ function cancelAddBook(){
 
 addBookForm();
 cancelAddBook();
+addBookToLibrary();
