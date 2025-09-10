f9b36d312707c4edc7f5a3004c9bac85f393c41c
majd anas <majdanas2004@gmail.com>
2025-09-10 17:20:22 +0300

    did the page skeleton did the banner and card styling created an object and array to store the books

---
+++ script.js
@@ -0,0 +1,24 @@
+const lib=[];
+let add=document.querySelector("#add");
+function Book(title,author,read,id){
+    this.title=title;
+    this.author=author;
+    this.id=id;
+    this.read=read;
+}
+
+
+function addBookToLibrary(title,author,read){
+    let book=new Book(title,author,read,crypto.randomUUID());
+    lib.push(book);
+}
+
+function displayLibrary(){
+    for(let book of lib){
+        //creat the cards
+    }
+}
+
+function addNewBook(){
+
+}
